9ac97afc2664df8bb89360e8b603b312c27d50c5
aleksandr-s-g <a.s.gavrilov94@gmail.com>
2023-09-23 17:53:52 +0400

    move to 33 android sdk version

--- android/build/build/generated/source/buildConfig/release/com/godot/game/BuildConfig.java
+++ android/build/build/generated/source/buildConfig/release/com/godot/game/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.cobragamesru.pinup";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 305;
-  public static final String VERSION_NAME = "1.2.2";
+  public static final int VERSION_CODE = 311;
+  public static final String VERSION_NAME = "1.2.3";
 }
